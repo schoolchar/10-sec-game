2c0ea343f8227cc82ca4eca90d7bf38d8b567e5d
schoolchar <br295143@ucf.edu>
2025-01-19 13:13:57 -0500

    timer weird

--- Assets/scripts/timer.cs
+++ Assets/scripts/timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class timer : MonoBehaviour
 {
@@ -11,36 +12,47 @@ public class timer : MonoBehaviour
     public GameObject lose;
     public GameObject start;
     public int time1 = -1;
+    public int started = 0;
 
     private void Start()
     {
         Instantiate(start);
+        start.SetActive(true);
+
     }
     // Update is called once per frame
     void Update()
     {
+        if (started == 0)
+        {
+            start.SetActive(true);
+        }
+        if (started == 1)
+        {
+            start.SetActive(false);
+        }
 
         if (time1 == -1)
         {
-            if (elapTime <= 2)
+            if (elapTime <= 2.00)
             {
+
+
                 elapTime += Time.deltaTime;
                 timerText.text = elapTime.ToString("F2");
+
+
             }
-        }
-
-
-
-
-        if (time1 == -1)
-        {
-            if (elapTime >= 2.00)
+            if (elapTime >= 1.99)
             {
+                started += 1;
                 time1 += 1;
-                elapTime = 0;
-                set (start, true);
+
+
+
             }
         }
+
 
       //  if (time1 == 0)
        // {
