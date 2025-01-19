daa21811a3c002132cf1e8a155b2b6343aa72bdf
schoolchar <br295143@ucf.edu>
2025-01-19 12:29:59 -0500

    thing

--- Assets/scripts/timer.cs
+++ Assets/scripts/timer.cs
@@ -8,13 +8,61 @@ public class timer : MonoBehaviour
     [SerializeField] TextMeshProUGUI timerText;
     private float time;
     private float elapTime;
+    public GameObject lose;
+    public GameObject start;
+    public int time1 = -1;
 
-
-
+    private void Start()
+    {
+        Instantiate(start);
+    }
     // Update is called once per frame
     void Update()
     {
-        elapTime += Time.deltaTime;
-        timerText.text = elapTime.ToString("F2");
+
+        if (time1 == -1)
+        {
+            if (elapTime <= 2)
+            {
+                elapTime += Time.deltaTime;
+                timerText.text = elapTime.ToString("F2");
+            }
+        }
+
+
+
+
+        if (time1 == -1)
+        {
+            if (elapTime >= 2.00)
+            {
+                time1 += 1;
+                elapTime = 0;
+                set (start, true);
+            }
+        }
+
+      //  if (time1 == 0)
+       // {
+      //      if (time <= 10)
+        //    {
+         //       elapTime += Time.deltaTime;
+       //         timerText.text = elapTime.ToString("F2");
+        //    }
+      //  }
+
+
+
+
+      //  if (time1 == 0)
+     //   {
+      //      if (elapTime >= 10.00)
+       //     {
+       //         time1 += 1;
+       //         Instantiate(lose);
+     //       }
+      //  }
+
+
     }
 }
