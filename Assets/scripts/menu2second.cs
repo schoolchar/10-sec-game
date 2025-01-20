0fa6b0598b4e49df0811f71669cd37d7e3299db1
schoolchar <br295143@ucf.edu>
2025-01-19 22:16:10 -0500

    done fr

--- Assets/scripts/menu2second.cs
+++ Assets/scripts/menu2second.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class menu2second : MonoBehaviour
 {
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource.Play();
         Invoke("newScene", 2f);
     }
 
