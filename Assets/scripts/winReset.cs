a73badf579ab17c658e6455db1e48fdab0620f78
schoolchar <br295143@ucf.edu>
2025-01-19 21:54:08 -0500

    s

--- Assets/scripts/winReset.cs
+++ Assets/scripts/winReset.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class winReset : MonoBehaviour
 {
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         Invoke("newScene", 2f);
+        audioSource.Play();
     }
 
     // Update is called once per frame
